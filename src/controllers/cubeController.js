335de49225073123147bfc137bfd2bd9f901bbf7
AlexTS3 <tsvetanovalex@yahoo.com>
2023-05-23 16:35:33 +0300

    Optimized handlebars and finished search and details pages

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -3,12 +3,12 @@ const router = require('express').Router();
 const cubeManager = require('../managers/cubeManager');
 
 router.get('/create', (req, res) => {
-    console.log(cubeManager.getAll())
     res.render('create');
 });
 
 router.post('/create', (req, res) => {
-    const { name,
+    const {
+        name,
         description,
         imageUrl,
         difficultyLevel,
@@ -22,6 +22,17 @@ router.post('/create', (req, res) => {
     });
 
     res.redirect('/')
+});
+
+router.get('/:cubeId/details', (req, res) => {
+    const cube = cubeManager.getOne(req.params.cubeId);
+    console.log(cube)
+
+    if (!cube) {
+        return res.redirect('/404');
+    };
+
+    res.render('details', { cube });
 })
 
 module.exports = router;
